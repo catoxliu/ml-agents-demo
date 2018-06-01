349e3e9a2e850bd50c39639026dc63786c573b88
catoxliu <catoxliu@tencent.com>
2018-06-01 22:04:53 +0800

    make bat file to start python istead of call it. add bat file to start tensorboard

---
+++ mlagent_python/board.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start python -m tensorboard.main --logdir summaries --host 127.0.0.1
--- mlagent_python/train.bat
+++ mlagent_python/train.bat
@@ -4,15 +4,16 @@ if "%~1" == "" GOTO HELP
 SET "ENV=%~1"
 if "%~2" == "" GOTO HELP
 SET "RUNID=%~2"
-
-rd /s /q "summaries/%RUNID%"
-rd /s /q "models/%RUNID%"
-python learn.py %ENV% --run-id=%RUNID% --train
-GOTO END
+GOTO RUN
 
 :HELP
 echo "should run with env and runid"
 echo "EXAMPLE: train.bat car car_1"
+GOTO :END
+
+:RUN
+rd /s /q "summaries/%RUNID%"
+rd /s /q "models/%RUNID%"
+start python learn.py %ENV% --run-id=%RUNID% --train
 
 :END
-exit /B
